3009d043318ba3b8f2d4e31d4578331a30da06be
Sanity <shpakjunior@yandex.ru>
2025-01-12 17:31:46 +0300

    Add files via upload

--- Testcase1/Testcase1.lua
+++ Testcase1/Testcase1.lua
@@ -7,140 +7,122 @@ function Testcase1:OnInitialize()
     self:Print("Testcase is work")
 end
 
-function Testcase1:ShowFrame()
-    MainFrame:Show()
-    MainFrame.Header.FAQ.Text:SetText("FAQ")
-    MainFrame.Header.MAIN.Text:SetText("ОСНОВНОЕ")
-    MainFrame.Header.Community.Text:SetText("СООБЩЕСТВО")
-    MainFrame.Header.SBIR.Text:SetText("СИСТЕМА БОЯ И РАЗВИТИЯ")
-    MainFrame.Header.Guidelines.Text:SetText("РУКОВОДСТВА")
-
-
-    MainFrame.Breadcrumbs.BreadcrumbForumThread.Text:SetText("Пользователи")
-    MainFrame.Breadcrumbs.BreadcrumbsForumThreadName.Text:SetText("SanityIs4TheWeak")
-    
-    MainFrame.Body.ContextUsername.Text:SetText("SanityIs4TheWeak")
-    TextWidth = (MainFrame.Body.ContextUsername.Text:GetWidth())
-    MainFrame.Body.ContextUsername:SetWidth(TextWidth)
-
-
-    MainFrame.Body.FriendsFrame.FriendsTag.Text:SetText("Друзья")
-    TextWidth = (MainFrame.Body.FriendsFrame.FriendsTag.Text:GetWidth())
-    MainFrame.Body.FriendsFrame.FriendsTag:SetWidth(TextWidth)
-
-    MainFrame.Body.CharactersFrame.CharactersTag.Text:SetText("Анкеты")
-    TextWidth = (MainFrame.Body.CharactersFrame.CharactersTag.Text:GetWidth())
-    MainFrame.Body.CharactersFrame.CharactersTag:SetWidth(TextWidth)
-
-    MainFrame.Body.EventsFrame.EventsTag.Text:SetText("События")
-    TextWidth = (MainFrame.Body.EventsFrame.EventsTag.Text:GetWidth())
-    MainFrame.Body.EventsFrame.EventsTag:SetWidth(TextWidth)
-
-    MainFrame.Body.PageWriteTag.Text:SetText("СТЕНА ПОЛЬЗОВАТЕЛЯ")
-    TextWidth = (MainFrame.Body.PageWriteTag.Text:GetWidth())
-    MainFrame.Body.PageWriteTag:SetWidth(TextWidth)
-
-
-
-    TextWidth = (MainFrame.Breadcrumbs.BreadcrumbForumThread.Text:GetWidth())
-    MainFrame.Breadcrumbs.BreadcrumbForumThread:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Breadcrumbs.BreadcrumbsForumThreadName.Text:GetWidth())
-    MainFrame.Breadcrumbs.BreadcrumbsForumThreadName:SetWidth(TextWidth)
-
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistration.Text:SetText("Регистрация")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistration.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistration:SetWidth(TextWidth)
-    
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistrationText.Text:SetText("6 лет назад")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistrationText.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistrationText:SetWidth(TextWidth)
-
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroups.Text:SetText("Группы")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroups.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroups:SetWidth(TextWidth)
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroupsText.Text:SetText("Ролевики")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroupsText.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroupsText:SetWidth(TextWidth)
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCity.Text:SetText("Город")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCity.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCity:SetWidth(TextWidth)
-
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCityText.Text:SetText("Брилл")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCityText.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCityText:SetWidth(TextWidth)
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSex.Text:SetText("Пол")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSex.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSex:SetWidth(TextWidth)
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSexText.Text:SetText("Мужской")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSexText.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSexText:SetWidth(TextWidth)
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkype.Text:SetText("Skype")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkype.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkype:SetWidth(TextWidth)
-
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkypeText.Text:SetText("123123123")
-    TextWidth = (MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkypeText.Text:GetWidth())
-    MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkypeText:SetWidth(TextWidth)
-
-    MainFrame.Body.ProfileNavigation.MyProfile.Text:SetText("ПРОФИЛЬ")
-    MainFrame.Body.ProfileNavigation.MyThread.Text:SetText("МОЯ ЛЕНТА")
-    MainFrame.Body.ProfileNavigation.MyFriends.Text:SetText("ДРУЗЬЯ")
-    MainFrame.Body.ProfileNavigation.MyComments.Text:SetText("КОММЕНТАРИИ")
-    MainFrame.Body.ProfileNavigation.MyReputation.Text:SetText("РЕПУТАЦИЯ")
-    MainFrame.Body.ProfileNavigation.MyIntegrations.Text:SetText("ИНТЕГРАЦИИ")
-
-    local TextWidth
-    TextWidth = (MainFrame.Body.ProfileNavigation.MyProfile.Text:GetWidth())
-    MainFrame.Body.ProfileNavigation.MyProfile:SetWidth(TextWidth)
-
-    local TextWidth
-    TextWidth = (MainFrame.Body.ProfileNavigation.MyThread.Text:GetWidth())
-    MainFrame.Body.ProfileNavigation.MyThread:SetWidth(TextWidth)
-
-    local TextWidth
-    TextWidth = (MainFrame.Body.ProfileNavigation.MyFriends.Text:GetWidth())
-    MainFrame.Body.ProfileNavigation.MyFriends:SetWidth(TextWidth)
-
-    local TextWidth
-    TextWidth = (MainFrame.Body.ProfileNavigation.MyComments.Text:GetWidth())
-    MainFrame.Body.ProfileNavigation.MyComments:SetWidth(TextWidth)
-
-    local TextWidth
-    TextWidth = (MainFrame.Body.ProfileNavigation.MyReputation.Text:GetWidth())
-    MainFrame.Body.ProfileNavigation.MyReputation:SetWidth(TextWidth)
-
-    local TextWidth
-    TextWidth = (MainFrame.Body.ProfileNavigation.MyIntegrations.Text:GetWidth())
-    MainFrame.Body.ProfileNavigation.MyIntegrations:SetWidth(TextWidth)
-
-
-    local TextWidth
-    TextWidth = (MainFrame.Header.FAQ.Text:GetWidth())
-    MainFrame.Header.FAQ:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Header.MAIN.Text:GetWidth())
-    MainFrame.Header.MAIN:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Header.Community.Text:GetWidth())
-    MainFrame.Header.Community:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Header.SBIR.Text:GetWidth())
-    MainFrame.Header.SBIR:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Header.Guidelines.Text:GetWidth())
-    MainFrame.Header.Guidelines:SetWidth(TextWidth)
+function ChangeText(object, Text)
+    object.Text:SetText(Text)
+    TextWidth = (object.Text:GetWidth())
+    object:SetWidth(TextWidth)
 end
 
+function Testcase1:ShowFrame()
+    MainFrame:Show()
 
--- function FrameWidthSetter(PathToElement)
--- local PathToText = PathToElement .. ".Text"
--- local TextWidth = (PathToText:GetWidth())
--- PathToElement:SetWidth(TextWidth)
--- end
+    ChangeText(MainFrame.Header.FAQ, "FAQ")
+    ChangeText(MainFrame.Header.MAIN, "ОСНОВНОЕ")
+    ChangeText(MainFrame.Header.Community, "СООБЩЕСТВО")
+    ChangeText(MainFrame.Header.SBIR, "СИСТЕМА БОЯ И РАЗВИТИЯ")
+    ChangeText(MainFrame.Header.Guidelines, "РУКОВОДСТВА")
+
+
+    ChangeText(MainFrame.Breadcrumbs.BreadcrumbForumThread, "Пользователи")
+    ChangeText(MainFrame.Breadcrumbs.BreadcrumbsForumThreadName, "SanityIs4TheWeak")
+    ChangeText(MainFrame.Body.ContextUsername, "SanityIs4TheWeak")
+    ChangeText(MainFrame.Body.FriendsFrame.FriendsTag, "Друзья")
+    ChangeText(MainFrame.Body.CharactersFrame.CharactersTag, "Анкеты")
+    ChangeText(MainFrame.Body.EventsFrame.EventsTag, "События")
+    ChangeText(MainFrame.Body.PageWriteTag, "СТЕНА ПОЛЬЗОВАТЕЛЯ")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistration, "Регистрация")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistrationText, "6 лет назад")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroups, "Группы")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroupsText, "Ролевики")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCity, "Город")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCityText, "Брилл")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSex, "Пол")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSexText, "Мужской")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkype, "Skype")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkypeText, "123123123")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyProfile, "ПРОФИЛЬ")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyThread, "МОЯ ЛЕНТА")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyFriends, "ДРУЗЬЯ")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyComments, "КОММЕНТАРИИ")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyReputation, "РЕПУТАЦИЯ")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyIntegrations, "ИНТЕГРАЦИИ")
 
 
 
+end
+
+function Testcase1:CreateRootItem()
+    local FriendsContainer = MainFrame.Body.FriendsFrame.FriendsItemContainer
+
+    local RootItem = CreateFrame("Frame", "Root", FriendsContainer, "FriendsItemTemplate")
+    RootItem:SetPoint("TOPLEFT", FriendsContainer, "TOPLEFT", 0, 0);
+    self.RootItem = RootItem
+    return RootItem
+end
+function Testcase1:GetItemCount(object)
+    local n = tonumber(object:GetText())
+    if n then
+        self:CreateFramesFromTemplate(n)
+    else
+        return nil
+    end
+end
+
+function Testcase1:CreateFramesFromTemplate(n)
+    local FriendsContainer = MainFrame.Body.FriendsFrame.FriendsItemContainer
+
+     local RootItem = self.RootItem
+    if not RootItem then
+        RootItem = self:CreateRootItem()
+    end
+	local items = {} 
+
+
+    local currentRow = 1;
+    local currentColumn = 0;
+    local previousItem = RootItem;
+
+    for i = 1, n do
+        if not items[currentRow] then
+            items[currentRow] = {}
+        end
+
+        local Item = CreateFrame("Frame", "Item" .. i, FriendsContainer, "FriendsItemTemplate")
+
+        if i == 1 then
+            Item:SetPoint("TOPLEFT", RootItem,"TOPLEFT", 0,0);
+             previousItem = Item;
+        else
+             Item:SetPoint("LEFT", previousItem, "RIGHT", 20, 0);
+              Item:SetPoint("TOP",previousItem,"TOP",0,0);
+              previousItem = Item;
+         end
+
+
+
+        items[currentRow][currentColumn] = Item
+        currentColumn = currentColumn + 1;
+
+
+        if currentColumn > 5 then
+
+            if currentRow >= 1 then
+                local currentHeight = MainFrame.Body.FriendsFrame:GetHeight()
+                MainFrame.Body.FriendsFrame:SetHeight(currentHeight + 35);
+                MainFrame.Body.FriendsFrame.Border:SetHeight(currentHeight + 35);
+                MainFrame:SetHeight(currentHeight + 35);
+            end;
+
+            currentColumn = 1
+            currentRow = currentRow + 1;
+            local previousRow = items[currentRow - 1]
+             if previousRow then
+                local firstInPreviousRow = previousRow[1];
+                Item:SetPoint("LEFT", RootItem, "LEFT", 0,0);
+                Item:SetPoint("TOP",firstInPreviousRow,"BOTTOM",0, -5);
+            end
+        end
+
+
+    end
+end
