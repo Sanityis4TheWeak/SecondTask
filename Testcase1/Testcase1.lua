6d357d8801c91633c703538b6459fe52e1e97a9b
Sanity <shpakjunior@yandex.ru>
2025-01-18 15:55:13 +0300

    Add files via upload

--- Testcase1/Testcase1.lua
+++ Testcase1/Testcase1.lua
@@ -1,43 +1,130 @@
 Testcase1 = LibStub("AceAddon-3.0"):NewAddon("Testcase1", "AceConsole-3.0", "AceEvent-3.0" );
 
+Testcase1.framePool = nil
+
 function Testcase1:OnInitialize()
 	self:RegisterChatCommand("testcase", "ShowFrame")
 	self:RegisterChatCommand("tc", "ShowFrame")
     self:RegisterChatCommand("tp", "ShowFrame")
     self:Print("Testcase is work")
+    local FriendsContainer = MainFrame.Body.FriendsFrame.FriendsItemContainer
+    self.framePool = CreateFramePool("Frame", FriendsContainer, "FriendsItemTemplate");
+
+end
+
+function ChangeText(object, Text)
+    object.Text:SetText(Text)
+    TextWidth = (object.Text:GetWidth())
+    object:SetWidth(TextWidth)
 end
 
 function Testcase1:ShowFrame()
     MainFrame:Show()
-    MainFrame.Header.FAQ.Text:SetText("FAQ")
-    MainFrame.Header.MAIN.Text:SetText("ОСНОВНОЕ")
-    MainFrame.Header.Community.Text:SetText("СООБЩЕСТВО")
-    MainFrame.Header.SBIR.Text:SetText("СИСТЕМА БОЯ И РАЗВИТИЯ")
-    MainFrame.Header.Guidelines.Text:SetText("РУКОВОДСТВА")
+
+    ChangeText(MainFrame.Header.FAQ, "FAQ")
+    ChangeText(MainFrame.Header.MAIN, "ОСНОВНОЕ")
+    ChangeText(MainFrame.Header.Community, "СООБЩЕСТВО")
+    ChangeText(MainFrame.Header.SBIR, "СИСТЕМА БОЯ И РАЗВИТИЯ")
+    ChangeText(MainFrame.Header.Guidelines, "РУКОВОДСТВА")
 
 
-    MainFrame.Breadcrumbs.BreadcrumbForumThread.Text:SetText("Пользователи")
-    MainFrame.Breadcrumbs.BreadcrumbsForumThreadName.Text:SetText("SanityIs4TheWeak")
-    TextWidth = (MainFrame.Breadcrumbs.BreadcrumbForumThread.Text:GetWidth())
-    MainFrame.Breadcrumbs.BreadcrumbForumThread:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Breadcrumbs.BreadcrumbsForumThreadName.Text:GetWidth())
-    MainFrame.Breadcrumbs.BreadcrumbsForumThreadName:SetWidth(TextWidth)
+    ChangeText(MainFrame.Breadcrumbs.BreadcrumbForumThread, "Пользователи")
+    ChangeText(MainFrame.Breadcrumbs.BreadcrumbsForumThreadName, "SanityIs4TheWeak")
+    ChangeText(MainFrame.Body.ContextUsername, "SanityIs4TheWeak")
+    ChangeText(MainFrame.Body.FriendsFrame.FriendsTag, "Друзья")
+    ChangeText(MainFrame.Body.CharactersFrame.CharactersTag, "Анкеты")
+    ChangeText(MainFrame.Body.EventsFrame.EventsTag, "События")
+    ChangeText(MainFrame.Body.PageWriteTag, "СТЕНА ПОЛЬЗОВАТЕЛЯ")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistration, "Регистрация")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileRegistrationText, "6 лет назад")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroups, "Группы")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileGroupsText, "Ролевики")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCity, "Город")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileCityText, "Брилл")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSex, "Пол")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSexText, "Мужской")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkype, "Skype")
+    ChangeText(MainFrame.Body.ProfileInformation.ProfileInformation.MyProfileSkypeText, "123123123")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyProfile, "ПРОФИЛЬ")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyThread, "МОЯ ЛЕНТА")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyFriends, "ДРУЗЬЯ")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyComments, "КОММЕНТАРИИ")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyReputation, "РЕПУТАЦИЯ")
+    ChangeText(MainFrame.Body.ProfileNavigation.MyIntegrations, "ИНТЕГРАЦИИ")
 
-    
 
-    local TextWidth
-    TextWidth = (MainFrame.Header.FAQ.Text:GetWidth())
-    MainFrame.Header.FAQ:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Header.MAIN.Text:GetWidth())
-    MainFrame.Header.MAIN:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Header.Community.Text:GetWidth())
-    MainFrame.Header.Community:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Header.SBIR.Text:GetWidth())
-    MainFrame.Header.SBIR:SetWidth(TextWidth)
-    TextWidth = (MainFrame.Header.Guidelines.Text:GetWidth())
-    MainFrame.Header.Guidelines:SetWidth(TextWidth)
+
+end
+
+
+FriendStatusMixin={};
+
+function FriendStatusMixin:FriendStatus()
+    local RandValue = math.random(1, 100);
+    if RandValue %2 == 0 then
+        local FriendStatus = true;
+        return FriendStatus
+    else 
+        FriendStatus = false;
+        return FriendStatus
+    end
+end
+
+function FriendStatusMixin:ShowOnlineTooltip()
+    if self:FriendStatus() == true then 
+        print("В  сети")
+    else
+        print("Не в  сети")
+    end
 end
 
 
 
 
+
+function Testcase1:CreateFriendsItem()
+    local Item = self.framePool:Acquire()
+    Item:Show()
+    print(Item)
+    return Item
+  end
+  
+  
+  
+  function Testcase1:GetItemCount(object)
+      local n = tonumber(object:GetText())
+      if n then
+          self:CreateFramesFromTemplate(n)
+      else
+          return nil
+      end
+  end
+  
+  function Testcase1:CreateFramesFromTemplate(n)
+    local FriendsContainer = MainFrame.Body.FriendsFrame.FriendsItemContainer
+    local ColumnCounter = 0
+    local yOffset = 0
+    local xOffset = 0
+
+    for i = 1, n do
+        local Item = self:CreateFriendsItem()
+        if not Item then
+          break
+        end
+
+        Item:SetPoint("TOPLEFT", FriendsContainer, "TOPLEFT", xOffset, yOffset)
+
+        xOffset = xOffset + 40
+        if i % 5 == 0 then
+            xOffset = 0
+            yOffset = yOffset - 40
+            ColumnCounter = ColumnCounter + 1
+        end
+    end
+
+    print(i)
+
+    local currentHeight = MainFrame.Body.FriendsFrame:GetHeight()
+    MainFrame.Body.FriendsFrame:SetHeight(currentHeight + 35 * ColumnCounter)
+    MainFrame.Body.FriendsFrame.Border:SetHeight(currentHeight + 35 * ColumnCounter)
+end
